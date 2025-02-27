b3b3bff68034c999d3b9f412c17e9bbc7d7a004c
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-02-27 07:47:58 -0800

    feat: add routing configuration and initial pages

---
+++ frontend/src/router.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+
+const AppRouter: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
